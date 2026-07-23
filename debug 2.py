803a65c50f94789276b0480bcec4e3689bb98a0c
cibinjohn <cibinjohnjoseph@gmail.com>
2026-07-23 15:06:08 -0400

    Create debug 2.py

---
+++ debug 2.py
@@ -0,0 +1,15 @@
+HOSTNAME = "nbcuni.sharepoint.com"
+SITE_PATH = "sites/skoonie/data"
+LIBRARY = "Shared Documents"
+FOLDER_PATH = "Advanced Analytics/Media Analytics/WIP/Miguel/Databricks - Sharepoint Test"
+
+site = await client.sites.by_site_id(f"{HOSTNAME}:/{SITE_PATH}:").get()
+print(site.display_name, site.id)
+
+drives = await client.sites.by_site_id(site.id).drives.get()
+drive_id = next(d.id for d in drives.value if d.name == LIBRARY)
+print(drive_id)
+
+folder_item = await client.drives.by_drive_id(drive_id).root.item_with_path(FOLDER_PATH).get()
+print(folder_item.id, folder_item.web_url)
+
